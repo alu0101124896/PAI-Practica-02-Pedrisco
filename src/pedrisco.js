ed4f16942169f5847066a7b412e9f12ec2025aea
Sergio Tabares <alu0101124896@ull.edu.es>
2020-02-13 23:37:20 +0000

    Pedrisco

--- src/pedrisco.js
+++ src/pedrisco.js
@@ -1,7 +1,7 @@
 if (process.argv.length !== 4 || isNaN(process.argv[2]) || isNaN(process.argv[3]) || Number(process.argv[2]) >= Number(process.argv[3])) {
   console.log('Error: Ejecute este programa aportando como argumentos en la linea de comandos dos numeros enteros, siendo el primero menor que el segundo.');
 } else {
-  let maxIters = 0, maxItersNum = 0, minIters = Number.MAX_VALUE, minItersNum = 0;
+  let maxIters = 0, maxItersNum = 0, minIters = Number.MAX_SAFE_INTEGER, minItersNum = 0;
   for (let i = Number(process.argv[2]); i <= Number(process.argv[3]); i++) {
     let iters = 0;
     let number = i;
