1f8b4b372418e687979d6f5120647089ae767b98
Sergio Tabares <alu0101124896@ull.edu.es>
2020-02-13 23:21:39 +0000

    Pedrisco

--- src/pedrisco.js
+++ src/pedrisco.js
@@ -1,8 +1,9 @@
 if (process.argv.length !== 4 || isNaN(process.argv[2]) || isNaN(process.argv[3]) || Number(process.argv[2]) >= Number(process.argv[3])) {
   console.log('Error: Ejecute este programa aportando como argumentos en la linea de comandos dos numeros enteros, siendo el primero menor que el segundo.');
 } else {
+  let maxIters = 0, maxItersNum = 0, minIters = Number.MAX_VALUE, minItersNum = 0;
   for (let i = Number(process.argv[2]); i <= Number(process.argv[3]); i++) {
-    let iterations = 0;
+    let iters = 0;
     let number = i;
     while (number !== 1) {
       if (number % 2 === 0) {
@@ -14,8 +15,18 @@ if (process.argv.length !== 4 || isNaN(process.argv[2]) || isNaN(process.argv[3]
         console.log("Error: El numero " + number + " no es par ni impar.");
         break;
       }
-      iterations++;
+      iters++;
     }
-    console.log(i + " - " + iterations);
+    if (iters > maxIters) {
+      maxIters = iters;
+      maxItersNum = i;
+    }
+    if (iters < minIters) {
+      minIters = iters;
+      minItersNum = i;
+    }
+    console.log(i + " - " + iters);
   }
+  console.log("El numero que requiere el mayor numero de iteraciones es el " + maxItersNum + " con un total de " + maxIters + " iteraciones.");
+  console.log("El numero que requiere el menor numero de iteraciones es el " + minItersNum + " con un total de " + minIters + " iteraciones.");
 }
